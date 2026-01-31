a4077eb5e53abd85307e5f5e23b2ee50b7649fe3
Barrett Poole <barrettpoole14@gmail.com>
2026-01-31 05:23:38 -0500

    Added View Resume Button

--- src/components/CtaLinks.jsx
+++ src/components/CtaLinks.jsx
@@ -5,6 +5,7 @@ export default function CtaLinks() {
         <div className='container'>
           <a className='cta cta-primary' href='#projects'>View Projects</a>
           <a className='cta cta-secondary' href='#contact-me'>Contact Me</a>
+          <a className="cta cta-primary" href="/Barrett_Poole_Frontend_Developer.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>
         </div>
     )
 }
